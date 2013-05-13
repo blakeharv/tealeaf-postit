5f8ce123766c570359e70d2eb71c7aefe99d07ec
Blake Harvey <blakeharvey@gmail.com>
2013-05-13 00:49:15 -0500

    add first migration

--- db/migrate/0001_create_posts.rb
+++ db/migrate/0001_create_posts.rb
@@ -1,7 +1,14 @@
 class CreatePosts < ActiveRecord::Migration
   def up
+    create_table :posts do |t|
+      t.string  :title
+      t.string  :url
+      t.text    :description
+
+      t.timestamps
   end
 
   def down
+    drop_table :products
   end
 end
